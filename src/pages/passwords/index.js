b094ca14c934119b788867c09b725e6a48843c41
vini-2205 <viniciuspinheiro2003@gmail.com>
2024-03-08 21:41:15 -0300

    Atualização na Página de Visualização de senhas

--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -1,13 +1,31 @@
 import {View, Text, StyleSheet} from 'react-native'
+import { SafeAreaView} from 'react-native-safe-area-context'
+import { useState, useEffect } from 'react'
+import { useIsFocused } from '@react-navigation/native'
 
 export function Passwords(){
+    
     return(
-        <View>
-            <Text>Minhas senhas</Text>
-        </View>
+        <SafeAreaView style ={{flex:1}}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Minhas senhas</Text>
+            </View>
+            
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    header:{
+        backgroundColor: "#392de9",
+        paddingTop: 58,
+        paddingBottom: 14,
+        paddingLeft: 14,
+        paddingRight: 14
+    },
+    title:{
+        fontSize: 18,
+        color: "#FFF",
+        fontWeight: 'bold'
+    }
 })
