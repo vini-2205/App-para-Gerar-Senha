70808eec68c807106e3f7b00c8b05b534ab7ef1a
vini-2205 <viniciuspinheiro2003@gmail.com>
2024-03-10 21:46:57 -0300

    Finalização do Projeto

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,13 +1,12 @@
 import { View, Text, StyleSheet, TouchableOpacity, Pressable } from "react-native";
 import * as Clipboard from 'expo-clipboard'
-import useStorage from '../../hooks/useStorage';
+import useStorage from '../../hooks/useStorage'
 
 export function ModalPassword({password, handleClose}){
-    const { saveItem } = useStorage;
+    const { saveItem } = useStorage();
 
     async function handleCopyPassword(){
         await Clipboard.setStringAsync(password)
-        alert("Senha salva com sucesso!")
         await saveItem("@pass", password)
         alert("Senha salva com sucesso!")
         handleClose();
@@ -28,7 +27,7 @@ export function ModalPassword({password, handleClose}){
                         <Text style={styles.buttonText}>Voltar</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={[styles.button, styles.buttonSave]}>
+                    <TouchableOpacity style={[styles.button, styles.buttonSave]} onPress={handleCopyPassword}>
                         <Text style={styles.buttonSaveText}>Salvar senha</Text>
                     </TouchableOpacity>
                 </View>
--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -1,16 +1,44 @@
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, FlatList} from 'react-native'
 import { SafeAreaView} from 'react-native-safe-area-context'
 import { useState, useEffect } from 'react'
 import { useIsFocused } from '@react-navigation/native'
+import useStorage from '../../hooks/useStorage'
+import { PasswordItem} from './components/passwordItem'
+
 
 export function Passwords(){
-    
+    const [listPasswords, setListPasswords] = useState([])
+    const focused = useIsFocused(); 
+    const { getItem, removeItem } = useStorage();
+
+    useEffect(() => {
+        async function loadPasswords(){
+            const passwords = await getItem("@pass")
+            setListPasswords(passwords);
+        }
+
+        loadPasswords();
+    }, [focused])
+
+    async function handleDeletePasswrd(item){
+        const passwords = await removeItem("@pass", item)
+        setListPasswords(passwords)
+    }
+
     return(
         <SafeAreaView style ={{flex:1}}>
             <View style={styles.header}>
                 <Text style={styles.title}>Minhas senhas</Text>
             </View>
             
+            <View style = {styles.content}>
+                <FlatList
+                    style={{flex:1, paddingTop: 14}}
+                    data = {listPasswords}
+                    keyExtractor={ (item) => String(item) }
+                    renderItem={ ({item}) => <PasswordItem data={item} removePassword={ () => handleDeletePasswrd(item)} />}
+                />
+            </View>
         </SafeAreaView>
     )
 }
@@ -27,5 +55,10 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: "#FFF",
         fontWeight: 'bold'
+    },
+    content:{
+        flex:1,
+        paddingLeft:14,
+        paddingRight:14
     }
 })
